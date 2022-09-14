a11cd46ef661f8669f7e1cc7c78a088036a05d1f
Jim DuValeus <jimduvaleus@yahoo..com>
2022-09-14 09:31:50 -0400

    updated to match example

--- modules/aws-s3-bucket/versions.tf
+++ modules/aws-s3-bucket/versions.tf
@@ -4,12 +4,6 @@
   *      reason... Consumers of the module can set more restrictive
   *      constraints. -->
 */
-/**
-  * <!-- Include all providers and version constraints for this module here.
-  *      Don't be too restrictive with the versions unless you have a good
-  *      reason... Consumers of the module can set more restrictive
-  *      constraints. -->
-*/
 
 terraform {
   required_providers {
